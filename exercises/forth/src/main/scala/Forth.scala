70333dc49f2f5e426a42e37a58f496f155a2eba9
Vladimir Korenev <vkorenev@gmail.com>
2019-01-15 20:48:02 -0800

    Forth: add support for numeric literals and built-in words

--- exercises/forth/src/main/scala/Forth.scala
+++ exercises/forth/src/main/scala/Forth.scala
@@ -1,6 +1,88 @@
 import ForthError.ForthError
 
-
 class Forth extends ForthEvaluator {
-  def eval(text: String): Either[ForthError, ForthEvaluatorState] = ???
+  import Forth._
+
+  def eval(text: String): Either[ForthError, ForthEvaluatorState] = {
+    val words = text.split("\\s+").view.map(_.toUpperCase).toList
+
+    words.foldLeft[Result[ForthState]](Right(initialState)) { (st, word) =>
+      for {
+        state <- st
+        definition <- state.lookup(word)
+        newState <- execute(definition)(state)
+      } yield newState
+    }
+  }
+}
+
+object Forth {
+  type Result[A] = Either[ForthError, A]
+
+  sealed trait Definition
+
+  case class NumLiteral(value: Int) extends Definition
+
+  case class BuiltIn(exec: ForthState => Result[ForthState]) extends Definition
+
+  case class ForthState(stack: List[Int], dictionary: Map[String, Definition]) extends ForthEvaluatorState {
+    private[this] def isNumLiteral(word: String) = word.forall(_.isDigit)
+
+    def lookup(word: String): Result[Definition] =
+      if (isNumLiteral(word)) {
+        Right(NumLiteral(word.toInt))
+      } else {
+        dictionary.get(word).toRight(ForthError.UnknownWord)
+      }
+
+    override def toString: String = stack.reverse.mkString(" ")
+  }
+
+  def modifyStack(exec: List[Int] => Result[List[Int]]): ForthState => Result[ForthState] = { state =>
+    for { newStack <- exec(state.stack) } yield state.copy(stack = newStack)
+  }
+
+  def execute(definition: Definition): ForthState => Result[ForthState] = definition match {
+    case NumLiteral(value) => modifyStack(stack => Right(value :: stack))
+    case BuiltIn(exec)     => exec
+  }
+
+  private val initialState = ForthState(
+    List.empty,
+    Map[String, Definition](
+      "+" -> BuiltIn(modifyStack {
+        case a :: b :: rest => Right(a + b :: rest)
+        case _              => Left(ForthError.StackUnderflow)
+      }),
+      "-" -> BuiltIn(modifyStack {
+        case a :: b :: rest => Right(b - a :: rest)
+        case _              => Left(ForthError.StackUnderflow)
+      }),
+      "*" -> BuiltIn(modifyStack {
+        case a :: b :: rest => Right(a * b :: rest)
+        case _              => Left(ForthError.StackUnderflow)
+      }),
+      "/" -> BuiltIn(modifyStack {
+        case 0 :: _ :: _    => Left(ForthError.DivisionByZero)
+        case a :: b :: rest => Right(b / a :: rest)
+        case _              => Left(ForthError.StackUnderflow)
+      }),
+      "DUP" -> BuiltIn(modifyStack {
+        case stack @ a :: _ => Right(a :: stack)
+        case _              => Left(ForthError.StackUnderflow)
+      }),
+      "DROP" -> BuiltIn(modifyStack {
+        case _ :: rest => Right(rest)
+        case _         => Left(ForthError.StackUnderflow)
+      }),
+      "SWAP" -> BuiltIn(modifyStack {
+        case a :: b :: rest => Right(b :: a :: rest)
+        case _              => Left(ForthError.StackUnderflow)
+      }),
+      "OVER" -> BuiltIn(modifyStack {
+        case stack @ _ :: b :: _ => Right(b :: stack)
+        case _                   => Left(ForthError.StackUnderflow)
+      })
+    )
+  )
 }
--- exercises/forth/src/test/scala/ForthTest.scala
+++ exercises/forth/src/test/scala/ForthTest.scala
@@ -10,153 +10,123 @@ class ForthTest extends FunSuite with Matchers {
   }
 
   test("addition - can add two numbers") {
-    pending
     forth.eval("1 2 +").fold(_ => "", _.toString) should be("3")
   }
 
   test("addition - errors if there is nothing on the stack") {
-    pending
     forth.eval("+").isLeft should be(true)
   }
 
   test("addition - errors if there is only one value on the stack") {
-    pending
     forth.eval("1 +").isLeft should be(true)
   }
 
   test("subtraction - can subtract two numbers") {
-    pending
     forth.eval("3 4 -").fold(_ => "", _.toString) should be("-1")
   }
 
   test("subtraction - errors if there is nothing on the stack") {
-    pending
     forth.eval("-").isLeft should be(true)
   }
 
   test("subtraction - errors if there is only one value on the stack") {
-    pending
     forth.eval("1 -").isLeft should be(true)
   }
 
   test("multiplication - can multiply two numbers") {
-    pending
     forth.eval("2 4 *").fold(_ => "", _.toString) should be("8")
   }
 
   test("multiplication - errors if there is nothing on the stack") {
-    pending
     forth.eval("*").isLeft should be(true)
   }
 
   test("multiplication - errors if there is only one value on the stack") {
-    pending
     forth.eval("1 *").isLeft should be(true)
   }
 
   test("division - can divide two numbers") {
-    pending
     forth.eval("12 3 /").fold(_ => "", _.toString) should be("4")
   }
 
   test("division - performs integer division") {
-    pending
     forth.eval("8 3 /").fold(_ => "", _.toString) should be("2")
   }
 
   test("division - errors if dividing by zero") {
-    pending
     forth.eval("4 0 /").isLeft should be(true)
   }
 
   test("division - errors if there is nothing on the stack") {
-    pending
     forth.eval("/").isLeft should be(true)
   }
 
   test("division - errors if there is only one value on the stack") {
-    pending
     forth.eval("1 /").isLeft should be(true)
   }
 
   test("combined arithmetic - addition and subtraction") {
-    pending
     forth.eval("1 2 + 4 -").fold(_ => "", _.toString) should be("-1")
   }
 
   test("combined arithmetic - multiplication and division") {
-    pending
     forth.eval("2 4 * 3 /").fold(_ => "", _.toString) should be("2")
   }
 
   test("dup - copies a value on the stack") {
-    pending
     forth.eval("1 dup").fold(_ => "", _.toString) should be("1 1")
   }
 
   test("dup - copies the top value on the stack") {
-    pending
     forth.eval("1 2 dup").fold(_ => "", _.toString) should be("1 2 2")
   }
 
   test("dup - errors if there is nothing on the stack") {
-    pending
     forth.eval("dup").isLeft should be(true)
   }
 
   test("drop - removes the top value on the stack if it is the only one") {
-    pending
     forth.eval("1 drop").fold(_ => "", _.toString) should be("")
   }
 
   test("drop - removes the top value on the stack if it is not the only one") {
-    pending
     forth.eval("1 2 drop").fold(_ => "", _.toString) should be("1")
   }
 
   test("drop - errors if there is nothing on the stack") {
-    pending
     forth.eval("drop").isLeft should be(true)
   }
 
   test("swap - swaps the top two values on the stack if they are the only ones") {
-    pending
     forth.eval("1 2 swap").fold(_ => "", _.toString) should be("2 1")
   }
 
   test(
     "swap - swaps the top two values on the stack if they are not the only ones") {
-    pending
     forth.eval("1 2 3 swap").fold(_ => "", _.toString) should be("1 3 2")
   }
 
   test("swap - errors if there is nothing on the stack") {
-    pending
     forth.eval("swap").isLeft should be(true)
   }
 
   test("swap - errors if there is only one value on the stack") {
-    pending
     forth.eval("1 swap").isLeft should be(true)
   }
 
   test("over - copies the second element if there are only two") {
-    pending
     forth.eval("1 2 over").fold(_ => "", _.toString) should be("1 2 1")
   }
 
   test("over - copies the second element if there are more than two") {
-    pending
     forth.eval("1 2 3 over").fold(_ => "", _.toString) should be("1 2 3 2")
   }
 
   test("over - errors if there is nothing on the stack") {
-    pending
     forth.eval("over").isLeft should be(true)
   }
 
   test("over - errors if there is only one value on the stack") {
-    pending
     forth.eval("1 over").isLeft should be(true)
   }
 
@@ -215,24 +185,20 @@ class ForthTest extends FunSuite with Matchers {
   }
 
   test("case-insensitivity - DUP is case-insensitive") {
-    pending
     forth.eval("1 DUP Dup dup").fold(_ => "", _.toString) should be("1 1 1 1")
   }
 
   test("case-insensitivity - DROP is case-insensitive") {
-    pending
     forth.eval("1 2 3 4 DROP Drop drop").fold(_ => "", _.toString) should be(
       "1")
   }
 
   test("case-insensitivity - SWAP is case-insensitive") {
-    pending
     forth.eval("1 2 SWAP 3 Swap 4 swap").fold(_ => "", _.toString) should be(
       "2 3 4 1")
   }
 
   test("case-insensitivity - OVER is case-insensitive") {
-    pending
     forth.eval("1 2 OVER Over over").fold(_ => "", _.toString) should be(
       "1 2 1 2 1")
   }
